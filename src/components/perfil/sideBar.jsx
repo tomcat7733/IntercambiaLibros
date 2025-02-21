3f47cb7b6b48f0176ca29f7977aff3186f358e75
JUAN MANUEL <juandev.sp@gmail.com>
2025-02-21 12:53:54 +0100

    Maquetacion 21-02-2025
    Seguimos con la maquetacion y creacion de componentes jsx

---
+++ src/components/perfil/sideBar.jsx
@@ -0,0 +1,45 @@
+<div className="container-fluid mt-5">
+  <div className="row">
+    <div className="col-2">
+      {/* SIDE NAV */}
+      <ul className="nav flex-column">
+        <li className="nav-item">
+          <a className="nav-link active text-body-secondary py-1" aria-current="page" href="#">
+            Mis datos
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-body-secondary py-1" href="#">
+            Mensajes
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-body-secondary py-1" href="#">
+            Opiniones
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-body-secondary py-1" href="#">
+            Mis libros
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-body-secondary py-1" href="#">
+            Transacciones
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-body-secondary py-1" href="#">
+            Lista de deseados
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-body-secondary py-1" href="#">
+            Sistema de reporte
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div className="col-8"></div>
+  </div>
+</div>;
