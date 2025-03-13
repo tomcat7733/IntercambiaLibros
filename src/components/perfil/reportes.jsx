540dc9bcd652150b8b5e7f663618468a6c28cdd5
JUAN MANUEL <juandev.sp@gmail.com>
2025-03-13 20:18:27 +0100

    Validacion de formulario y contacto en reportes.jsx

--- src/components/perfil/reportes.jsx
+++ src/components/perfil/reportes.jsx
@@ -1,8 +1,82 @@
 'use client';
+import { useState } from 'react';
 import SideBar from '@components/perfil/sideBar';
 import Image from 'next/image';
 
 export default function Reportes() {
+  // Estado para almacenar los datos del formulario y errores
+  const [formValues, setFormValues] = useState({
+    nombre: '',
+    apellidos: '',
+    email: '',
+    motivo: '',
+    mensaje: '',
+    acepto: false,
+  });
+  const [errors, setErrors] = useState({});
+
+  // Maneja cambios en el formulario
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormValues((prev) => ({
+      ...prev,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
+
+  // Función de validación
+  const validate = () => {
+    const newErrors = {};
+    const msg = 'Este campo es obligatorio.';
+
+    if (!formValues.nombre.trim()) {
+      newErrors.nombre = requiredMsg;
+    } else if (formValues.nombre.trim().length < 3) {
+      newErrors.nombre = 'El nombre debe contener al menos 3 caracteres.';
+    }
+    if (!formValues.apellidos.trim()) {
+      newErrors.apellidos = requiredMsg;
+    } else if (formValues.apellidos.trim().length < 3) {
+      newErrors.apellidos = 'El apellido debe contener al menos 3 caracteres.';
+    }
+
+    if (!formValues.email.trim()) {
+      newErrors.email = msg;
+    } else if (!/\S+@\S+\.\S+/.test(formValues.email)) {
+      newErrors.email = 'El email no es válido.';
+    }
+
+    if (!formValues.mensaje.trim()) {
+      newErrors.mensaje = requiredMsg;
+    } else if (formValues.mensaje.trim().length < 20) {
+      newErrors.mensaje =
+        'El mensaje debe contener al menos 20 caracteres para explicar detalladamente el motivo. Por favor, proporciona una explicación completa.';
+    }
+
+    if (!formValues.mensaje.trim()) newErrors.mensaje = msg;
+
+    if (!formValues.acepto) {
+      newErrors.acepto = 'Debes aceptar los términos y condiciones.';
+    }
+    // Se devuelven los errores
+    return newErrors;
+  };
+
+  // Manejar el envío del formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+    // Si no hay errores, puedes proceder a enviar los datos al servidor
+    setErrors({});
+    console.log('Formulario válido:', formValues);
+    // Aqui hacemos el call a la api
+  };
+
   return (
     <div className="container-fluid">
       <div className="row g-0">
@@ -72,39 +146,81 @@ export default function Reportes() {
 
               {/* Formulario */}
               <h4 className="mt-5">Contacto</h4>
-              <form className="row g-3 mb-5 mt-2">
+              <form className="row g-3 mb-5 mt-2" onSubmit={handleSubmit}>
                 <div className="form-floating col-md-6">
-                  <input type="text" className="form-control" id="floatingInputNombre" placeholder="Nombre" />
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="floatingInputNombre"
+                    placeholder="Nombre"
+                    name="nombre"
+                    onChange={handleChange}
+                  />
                   <label htmlFor="floatingInputNombre">Nombre</label>
+                  {errors.nombre && <small className="text-danger">{errors.nombre}</small>}
                 </div>
                 <div className="form-floating col-md-6">
-                  <input type="text" className="form-control" id="floatingApellidos" placeholder="Apellidos" />
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="floatingApellidos"
+                    placeholder="Apellidos"
+                    name="apellidos"
+                    onChange={handleChange}
+                  />
                   <label htmlFor="floatingApellidos">Apellidos</label>
+                  {errors.apellidos && <small className="text-danger">{errors.apellidos}</small>}
                 </div>
                 <div className="form-floating col-12">
-                  <input type="email" className="form-control" id="floatingEmail" placeholder="Email" />
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="floatingEmail"
+                    placeholder="Email"
+                    name="email"
+                    onChange={handleChange}
+                  />
                   <label htmlFor="floatingEmail">Email</label>
+                  {errors.email && <small className="text-danger">{errors.email}</small>}
                 </div>
                 <div className="form-floating col-12">
-                  <input type="text" className="form-control" id="floatingReason" placeholder="Motivo" />
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="floatingReason"
+                    placeholder="Motivo"
+                    name="motivo"
+                    onChange={handleChange}
+                  />
                   <label htmlFor="floatingReason">Título del mensaje</label>
+                  {errors.motivo && <small className="text-danger">{errors.motivo}</small>}
                 </div>
                 <div className="form-floating col-12 my-3">
                   <textarea
                     className="form-control"
                     id="floatingInputDescripcion"
                     placeholder="Mensaje"
+                    name="mensaje"
                     style={{ height: '100px' }}
+                    onChange={handleChange}
                   />
                   <label htmlFor="floatingInputDescripcion">Mensaje</label>
+                  {errors.mensaje && <small className="text-danger">{errors.mensaje}</small>}
                 </div>
                 <div className="col-12">
                   <div className="form-check">
-                    <input className="form-check-input" type="checkbox" id="gridCheck" />
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      id="gridCheck"
+                      name="acepto"
+                      onChange={handleChange}
+                    />
                     <label className="form-check-label" htmlFor="gridCheck">
                       Acepto los términos y condiciones
                     </label>
                   </div>
+                  {errors.acepto && <small className="text-danger">{errors.acepto}</small>}
                 </div>
                 <div className="col-12">
                   <button type="submit" className="btn btn-primary">
