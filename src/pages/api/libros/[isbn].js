395e28c6777b150fcf9d75d9cc7912173b2d885b
tomcat7733 <tomcat7733@gmail.com>
2025-03-26 08:05:09 +0100

    cambio api

--- src/pages/api/libros/[isbn].js
+++ src/pages/api/libros/[isbn].js
@@ -10,7 +10,9 @@ export default async function handler(req, res) {
       .eq('isbn', isbn)
       .single();
 
-    if (error) return res.status(404).json({ error: error.message });
+    if (error) {
+      return res.status(404).json({ error: error.message });
+    }
 
     return res.status(200).json(data);
   }
